cfce2848f8245ddc1bbc151a204286e4ee390df5
dimidagd <dimidagd@gmail.com>
2019-11-30 16:38:09 +0100

    "changes to result plotting to reflect each model"
    Signed-off-by: dimidagd <dimidagd@gmail.com>

--- post_processing/plot_mean_IOU_history.py
+++ post_processing/plot_mean_IOU_history.py
@@ -60,30 +60,32 @@ for iter, item in enumerate(fold_list):
 which_model = models[3]
 hist = histories[which_model]
 
-ax = plt.subplot(1,1,1)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 20))
+
 p = []
 colors = np.array(list(color2label.keys()))/255
 for iter,cl in enumerate(classes):
-    plt.plot(100*filter(inp=hist[:,cl]), label=str(cl),color=colors[cl])
-ax.set_title(which_model+ " class IoU", fontweight="bold")
-ax.set_xlabel("epoch")
-ax.set_ylabel("IoU %")
+    data = 100*filter(inp=hist[:,cl])
 
-
+    ax1.plot(data, label=str(cl),color=colors[cl],linewidth=6)
+ax1.set_title("Training",fontsize=30)
+ax1.set_xlabel("epoch",fontsize=30)
+ax1.set_ylabel("IoU %",fontsize=30)
+ax1.tick_params(labelsize=20)
 #Beautify
-ax.spines["top"].set_visible(False)
-ax.spines["bottom"].set_visible(False)
-ax.spines["right"].set_visible(False)
-ax.spines["left"].set_visible(False)
+ax1.spines["top"].set_visible(False)
+ax1.spines["bottom"].set_visible(False)
+ax1.spines["right"].set_visible(False)
+ax1.spines["left"].set_visible(False)
 
 r1,r2 = plt.xlim()
-r = (int(r1), int(r2))
+r = (50, 300)
 for y in range(10, 91, 10):
-    plt.plot(range(*r), [y] * len(range(*r)), "--", lw=0.5, color="black", alpha=0.3)
+    ax1.plot(range(*r), [y] * len(range(*r)), "--", lw=0.5, color="black", alpha=0.3)
 
-plt.savefig(os.path.join("files","Single_model_IoU_training.pdf"), bbox_inches="tight")
-plt.show()
-plt.close()
+# plt.savefig(os.path.join("files","Single_model_IoU_training.pdf"), bbox_inches="tight")
+# plt.show()
+# plt.close()
 
 
 
@@ -91,7 +93,8 @@ plt.close()
 ## BARH PLOT
 width = 0.35
 plt.rcdefaults()
-fig, ax = plt.subplots(figsize=(5, 10))
+
+
 people = np.array(list(label2color.keys()))
 colors = np.array(list(color2label.keys()))/255
 y_pos = np.arange(len(people))
@@ -112,28 +115,29 @@ colors = colors[filter_empty_nan]
 y_pos = np.arange(len(colors))
 
 
-ax.barh(y_pos, 100*performance,height=0.8, align='center',color=colors,zorder=10)
-ax.set_yticks(y_pos)
-ax.set_yticklabels(people)
+ax2.barh(y_pos, 100*performance,height=0.8, align='center',color=colors,zorder=10)
+ax2.set_yticks(y_pos)
+ax2.set_yticklabels(people)
+ax2.tick_params(labelsize=20)
 
-ax.set_xlabel('IoU')
-ax.set_title(which_model+' IoU accuracy', fontweight="bold")
-plt.ylim(min(y_pos)+0.5, max(y_pos)-0.5)
+ax2.set_xlabel('IoU %',fontsize=30)
+ax2.set_title("Final",fontsize=30)
+ax2.set_ylim(min(y_pos)-0.5, max(y_pos)+0.5)
 
 
 #Beautify
-ax.spines["top"].set_visible(False)
-ax.spines["bottom"].set_visible(False)
-ax.spines["right"].set_visible(False)
-ax.spines["left"].set_visible(False)
+ax2.spines["top"].set_visible(False)
+ax2.spines["bottom"].set_visible(False)
+ax2.spines["right"].set_visible(False)
+ax2.spines["left"].set_visible(False)
 
 
 r1,r2 = plt.ylim()
 r = (int(r1-0.5), int(r2+0.5))
 for y in range(1, 90, 10):
-    plt.plot([y] * len(range(*r)),range(*r), "--", lw=0.5, color="black", alpha=0.3,zorder=1)
+    ax2.plot([y] * len(range(*r)),range(*r), "--", lw=0.5, color="black", alpha=0.3,zorder=1)
 
-ax.invert_yaxis()  # labels read top-to-bottom
+ax2.invert_yaxis()  # labels read top-to-bottom
 
 plt.savefig(os.path.join("files","Single_model_IoU_classes_colored.pdf"), bbox_inches="tight")
 plt.show()
@@ -165,8 +169,8 @@ plt.barh(y_pos + width, 100*np.max(histories[models[6]], axis=0)[sorting_filter_
 
 plt.yticks(y_pos,people)
 ax.set_title("Model comparison on IoU", fontweight="bold")
-ax.set_xlabel("IoU %")
-plt.legend(loc='best')
+ax.set_xlabel("IoU %",fontsize=20)
+ax.legend(loc='best')
 
 
 #Beautify
@@ -182,7 +186,7 @@ ax.get_yaxis().tick_left()
 r1,r2 = plt.ylim()
 r = (int(r1-0.5), int(r2+0.5))
 for y in range(1, 90, 10):
-    plt.plot([y] * len(range(*r)),range(*r), "--", lw=0.5, color="black", alpha=0.3,zorder=1)
+    ax.plot([y] * len(range(*r)),range(*r), "--", lw=0.5, color="black", alpha=0.3,zorder=1)
 
 ax.invert_yaxis()  # labels read top-to-bottom
 
@@ -214,17 +218,17 @@ ax = plt.subplot(1,1,1)
 p = []
 for iter,mdl in enumerate(models):
     if histories[mdl][-1]> 0.4:
-        plt.plot(100 * filter(inp=histories[mdl]), label=str(mdl))
+        ax.plot(100 * filter(inp=histories[mdl]), label=str(mdl),linewidth=4)
 
 ax.set_title("Model pixel accuracy", fontweight="bold")
 ax.set_xlabel("epoch")
-ax.set_ylabel("Px accuracy %")
+ax.set_ylabel("Pixel accuracy %")
 
 
 r1,r2 = plt.xlim()
 r = (int(r1), int(r2))
 for y in range(10, 91, 10):
-    plt.plot(range(*r), [y] * len(range(*r)), "--", lw=0.5, color="black", alpha=0.3)
+    ax.plot(range(*r), [y] * len(range(*r)), "--", lw=0.5, color="black", alpha=0.3)
 
 
 plt.legend(loc='best')
@@ -262,12 +266,12 @@ for iter, item in enumerate(fold_list):
 ax = plt.subplot(1,1,1)
 p = []
 for iter,mdl in enumerate(models):
-    plt.plot(histories[mdl], label=mdl,zorder=10)
+    ax.plot(histories[mdl], label=mdl,zorder=10,linewidth=3)
 
 
-ax.set_title("Learning rate", fontweight="bold")
-ax.set_xlabel("epoch")
-ax.set_ylabel("lr")
+ax.set_title("Learning rate",fontsize=25)
+ax.set_xlabel("epoch",fontsize=15)
+ax.set_ylabel("lr",fontsize=15)
 
 #
 r1,r2 = plt.xlim()
@@ -276,7 +280,7 @@ r = (int(r1), int(r2))
 r1y, r2y = plt.ylim()
 
 for y in list(plt.yticks()[0][1:-1]):
-    plt.plot(range(*r), [y] * len(range(*r)), "--", lw=0.5, color="black", alpha=0.3,zorder=1)
+    ax.plot(range(*r), [y] * len(range(*r)), "--", lw=0.5, color="black", alpha=0.3,zorder=1)
 
 
 
@@ -290,5 +294,124 @@ ax.get_yaxis().tick_left()
 plt.legend(loc='best')
 
 plt.savefig(os.path.join("files","Learning_rate.pdf"), bbox_inches="tight")
+plt.show()
+plt.close()
+
+
+# Combine last 3 =====================
+
+fig1 = plt.figure()
+# fig2 = plt.figure()  # more figures are easily accessible
+# fig3 = plt.figure()  # more figures are easily accessible
+
+ax11 = fig1.add_subplot(221)  # add subplot into first position in a 2x2 grid (upper left)
+ax12 = fig1.add_subplot(223, sharex=ax11)  # add to third position in 2x2 grid (lower left) and sharex with ax11
+ax13 = fig1.add_subplot(122)  # add subplot to cover both upper and lower right, in a 2x2 grid. This is the same as the rightmost panel in a 1x2 grid.
+# ax21 = fig2.add_subplot(211)  # add axes to the extra figures
+# ax21 = fig2.add_subplot(212)  # add axes to the extra figures
+# ax31 = fig3.add_subplot(111)  # add axes to the extra figures
+
+
+
+# Learning rate
+
+histories = {}
+models = []
+for iter, item in enumerate(fold_list):
+    # print(str(iter)+'. ' + item)
+    file_path = os.path.join(scores_dir, item, 'learning_rate.npy')
+    file_path2 = os.path.join(scores_dir, item, 'meanPixel.npy')
+    check_if_good_one = np.load(file_path2)
+    which_model = item.partition("-")[0]
+    if not item[-4:] == 'True':
+        models.append(which_model)
+        hist = np.load(file_path)
+        hist = hist[:np.max(np.nonzero(hist))]  # Crop until trained epoch
+        histories[which_model] = hist
+
+
+p = []
+for iter,mdl in enumerate(models):
+    ax11.plot(histories[mdl], label=mdl,zorder=10,linewidth=3)
+
+
+ax11.set_title("Learning rate",fontsize=10)
+ax11.set_xlabel("epoch",fontsize=10)
+ax11.set_ylabel("lr",fontsize=10)
+
+#
+r1,r2 = ax11.get_xlim()
+r = (int(r1), int(r2))
+
+r1y, r2y = ax11.get_ylim()
+
+for y in list(ax11.get_yticks()[1:-1]):
+    ax11.plot(range(*r), [y] * len(range(*r)), "--", lw=0.5, color="black", alpha=0.3,zorder=1)
+
+
+
+#Beautify
+ax11.spines["top"].set_visible(False)
+ax11.spines["bottom"].set_visible(False)
+ax11.spines["right"].set_visible(False)
+ax11.spines["left"].set_visible(False)
+ax11.get_xaxis().tick_bottom()
+ax11.get_yaxis().tick_left()
+#ax11.legend(loc='best')
+
+
+
+
+# Pixel accuracy training
+histories = {}
+models = []
+for iter, item in enumerate(fold_list):
+    # print(str(iter)+'. ' + item)
+    file_path = os.path.join(scores_dir, item, 'meanPixel.npy')
+
+    which_model = item.partition("-")[0]
+    if item[-4:] == 'True': which_model = which_model + 'VGGtrained'
+    models.append(which_model)
+    hist = np.load(file_path)
+    hist = hist[:np.max(np.nonzero(hist))]  # Crop until trained epoch
+    histories[which_model] = hist
+
+
+
+p = []
+for iter,mdl in enumerate(models):
+    if histories[mdl][-1]> 0.4:
+        ax12.plot(100 * filter(inp=histories[mdl]), label=str(mdl),linewidth=4)
+
+ax12.set_title("Model pixel accuracy", fontweight="bold")
+ax12.set_xlabel("epoch")
+ax12.set_ylabel("Pixel accuracy %")
+
+
+#
+r1,r2 = ax12.get_xlim()
+r = (int(r1), int(r2))
+
+r1y, r2y = ax11.get_ylim()
+
+for y in list(ax11.get_yticks()[1:-1]):
+    ax12.plot(range(*r), [y] * len(range(*r)), "--", lw=0.5, color="black", alpha=0.3,zorder=1)
+
+
+
+plt.legend(loc='best')
+
+#Beautify
+ax12.spines["top"].set_visible(False)
+ax12.spines["bottom"].set_visible(False)
+ax12.spines["right"].set_visible(False)
+ax12.spines["left"].set_visible(False)
+ax12.get_xaxis().tick_bottom()
+ax12.get_yaxis().tick_left()
+
+
+
+
+
 plt.show()
 plt.close()
